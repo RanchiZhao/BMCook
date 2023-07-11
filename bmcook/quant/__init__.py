2ac984dd680bd58f8745ddfdf486902dcd3098d6
RanchiZhao <yuanqian_zhao@163.com>
2023-07-11 17:18:23 +0800

    int4

--- bmcook/quant/__init__.py
+++ bmcook/quant/__init__.py
@@ -43,10 +43,19 @@ class BMQuant:
                     else:
                         module.forward = types.MethodType(forward_in8, module)
                     module.quant = True
+        #add here            
         elif quant_config['is_quant'] == 'int4':
-            #将所有Linear层替换掉
-            #
-            pass
+            target_linear = model_center.layer.Linear if target_linear is None else target_linear
+            for name, module in model.named_modules():
+                if isinstance(module, target_linear):
+                    if len(quant_config["quantized_module"]) != 0:
+                        if not any([pattern in name for pattern in quant_config["quantized_module"]]):
+                            continue
+                    if target_linear != model_center.layer.Linear:
+                        module.forward = types.MethodType(forward_int4_cpmlive, module)
+                    else:
+                        raise FutureWarning("not available, future work") 
+                    module.quant = True
 
 
 def forward_in8(module_self, x):
@@ -72,23 +81,28 @@ def forward_int8_cpmlive(module_self, x):
     return x
 
 #add here
-def forward_int4_cpmlive(module_self, x: torch.Tensor):
-# weights are cast automatically as Int8Params, but the bias has to be cast manually
+def forward_int4_cpmlive(module_self, x: torch.Tensor,blocksize,compress_statistics,quant_type):
+    #quant_state可能需要保存
+    original_weights = module_self.weight.data
+    w = module_self.weight.data.contiguous().half()
+    w_4bit, quant_state = bnb.functional.quantize_4bit(w, blocksize=blocksize, compress_statistics=compress_statistics, quant_type=quant_type)
+
     if getattr(module_self.weight, 'quant_state', None) is None:
         print('quantization state not initialized. Please ensure that the model parameters you load include the quant_state attribute.')
     inp_dtype = x.dtype
-    dtype_dict = {
-        'torch.float32': torch.float32,
-        'torch.float16': torch.float16,
-    }
-    if module_self.compute_dtype is not None:
-        if isinstance(module_self.compute_dtype, str):
-            module_self.compute_dtype = dtype_dict[module_self.compute_dtype]
-        x = x.to(dtype=module_self.compute_dtype)
+    # dtype_dict = {
+    #     'torch.float32': torch.float32,
+    #     'torch.float16': torch.float16,
+    # }
+    # if module_self.compute_dtype is not None:
+    #     if isinstance(module_self.compute_dtype, str):
+    #         module_self.compute_dtype = dtype_dict[module_self.compute_dtype]
+    #     x = x.to(dtype=module_self.compute_dtype)
     if module_self.scale_before:
         x = x / math.sqrt(module_self.dim_in)
-    out = bnb.matmul_4bit(x, module_self.weight.t(), bias=None, quant_state=module_self.weight.quant_state)
+    out = bnb.matmul_4bit(x, w_4bit.t(), bias=None, quant_state=quant_state)
     out = out.to(inp_dtype)
     if not module_self.scale_before:
         x = x / math.sqrt(module_self.dim_in)
+    module_self.weight.data = original_weights
     return out
